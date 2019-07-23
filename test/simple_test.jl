793f555289872b13c64890f848ee6cc6b43f8319
Jordan Jalving <jhjalving@gmail.com>
2019-07-23 18:16:14 -0500

    updated test

--- test/simple_test.jl
+++ test/simple_test.jl
@@ -13,4 +13,6 @@ KaHyPar.partition(h,2,configuration = :edge_cut)
 
 KaHyPar.partition(h,2,configuration = :connectivity)
 
+KaHyPar.partition(h,2,configuration = joinpath(@__DIR__,"../src/config/km1_direct_kway_sea18.ini"))
+
 true
